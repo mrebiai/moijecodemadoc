6a3bfc29892df0d237e22023c755b66c002ad585
Rebiai Mehdi <mrebiai@sqli.com>
2015-10-11 00:20:34 +0200

    Fix README

--- worflow.groovy
+++ worflow.groovy
@@ -1,11 +1,11 @@
 final def WORKSPACE_SUFFIX = "/jenkins_home/jobs/test-adoc/workspace";
 final def WORKSPACE = "/srv" + WORKSPACE_SUFFIX;
 final def LOCAL_WORKSPACE = "/var" + WORKSPACE_SUFFIX;
-final def PROJECT_NAME = "mon-projet";
+final def PROJECT_NAME = "mon-wiki";
 final def GIT_PREFIX = "ssh://git@gitlab:22/bdxio/"+PROJECT_NAME;
 final def GIT_REPO = GIT_PREFIX + ".git";
 final def GIT_WIKI_REPO = GIT_PREFIX + ".wiki.git";
-final def CREDENTIALS = 'c30c9385-541e-4a34-99bd-36f6561da726';
+final def CREDENTIALS = 'xxxx';
 
 stage name:'Clean workspace'; 
 node {
@@ -27,10 +27,11 @@ try {
  
   stage name:'Generate doc';
   node {
-    def cmd1 = 'asciidoctor '+PROJECT_NAME+'/*.adoc -D ' + PROJECT_NAME+'.wiki/generated';
+    def cmd1 = 'asciidoctor -r asciidoctor-diagram '+PROJECT_NAME+'/*.adoc -D ' + PROJECT_NAME+'.wiki/generated';
     def cmd2 = 'chmod -R a+w '+PROJECT_NAME+'.wiki/generated'; 
     sh 'docker run --rm  -v '+WORKSPACE+':/documents/ asciidoctor/docker-asciidoctor '+cmd1;
     sh 'docker run --rm  -v '+WORKSPACE+':/documents/ asciidoctor/docker-asciidoctor '+cmd2;
+    // TODO update index.asciidoc
   }
   
   stage name:'Commit generated doc';
