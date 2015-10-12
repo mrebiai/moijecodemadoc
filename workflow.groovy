1a4b5803ae65c23477b3da263ef04e1af861974b
Rebiai Mehdi <mrebiai@sqli.com>
2015-10-12 22:22:24 +0200

    Impl update index.asciidoc

--- workflow.groovy
+++ workflow.groovy
@@ -1,44 +1,58 @@
-final def WORKSPACE_SUFFIX = "/jenkins_home/jobs/test-adoc/workspace";
-final def WORKSPACE = "/srv" + WORKSPACE_SUFFIX;
-final def LOCAL_WORKSPACE = "/var" + WORKSPACE_SUFFIX;
+// A définir
 final def PROJECT_NAME = "mon-wiki";
+final def CREDENTIALS = 'xxxx';
+
+// A ne pas modifier
+def LOCAL_WORKSPACE;
+node {
+	LOCAL_WORKSPACE = pwd();
+}
+final def WORKSPACE = LOCAL_WORKSPACE.replaceFirst("/var", "/srv");
 final def GIT_PREFIX = "ssh://git@gitlab:22/bdxio/"+PROJECT_NAME;
 final def GIT_REPO = GIT_PREFIX + ".git";
 final def GIT_WIKI_REPO = GIT_PREFIX + ".wiki.git";
-final def CREDENTIALS = 'xxxx';
 
-stage name:'Clean workspace'; 
+stage name:'CLEAN'; 
 node {
  sh 'rm -rf *';
 }
 
 try {
-
   stage name:'CHECKOUT';   
   node {
+  	// scrutation sur ce repo uniquement pour relancer le build du job
     dir(PROJECT_NAME) {
       git credentialsId: CREDENTIALS, url: GIT_REPO, branch: 'master';
     }
-    //dir(PROJECT_NAME+".wiki") {
-    //  git credentialsId: CREDENTIALS, url: GIT_WIKI_REPO, branch: 'master';
-    //}
+    // pas de scrutation sur ce repo
     sh "git clone "+GIT_WIKI_REPO;
   }
- 
-  stage name:'Generate doc';
+  stage name:'GENERATE';
   node {
+    // génération
     def cmd1 = 'asciidoctor -r asciidoctor-diagram '+PROJECT_NAME+'/*.adoc -D ' + PROJECT_NAME+'.wiki/generated';
+    // mise à jour des droits pour pouvoir vider le workspace en fin de job
     def cmd2 = 'chmod -R a+w '+PROJECT_NAME+'.wiki/generated'; 
     sh 'docker run --rm  -v '+WORKSPACE+':/documents/ asciidoctor/docker-asciidoctor '+cmd1;
     sh 'docker run --rm  -v '+WORKSPACE+':/documents/ asciidoctor/docker-asciidoctor '+cmd2;
-    // TODO update index.asciidoc
+    // update index.asciidoc
+    def output = "";  
+    dir(PROJECT_NAME) {
+      files = new File(pwd()).listFiles();
+      for (f in files) {
+        if (f.name.endsWith(".adoc")) {
+      		output+="* link:generated/"+(f.name.replaceFirst(".adoc", ".html"))+"[]\n";
+      	}
+      }
+    }
+    dir(PROJECT_NAME+".wiki") {
+      def fOutput = new File(pwd()+"/index.asciidoc");
+      fOutput.write(output); 
+    }
   }
-  
-  stage name:'Commit generated doc';
+  stage name:'PUBLISH';
   node {
     dir(PROJECT_NAME+".wiki") {  
-      //sh "git config user.name jenkins";
-      //sh "git config user.email some@email.com";
       sh "git add -A .";
       sh "git commit -m 'commit generated doc'";
       sh "git push -u origin master";
